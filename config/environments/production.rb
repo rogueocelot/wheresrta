2b8484543540dcecf68b07002b4e34b7854c8158
Eric <epasho@yahoo.com>
2024-03-09 02:03:00 -0500

    added other lines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "wheresrta.net"
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
